c7768df38549155015a942f7257cda62d1597155
Toluwalade Eyinade <ToluwaladeEyinade@kpmg.com.mt>
2021-01-05 16:20:45 +0100

    initial project structure

---
+++ src/Betsson.EscapeMines.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Betsson.EscapeMines.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Betsson.EscapeMines.Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Betsson.EscapeMines.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
