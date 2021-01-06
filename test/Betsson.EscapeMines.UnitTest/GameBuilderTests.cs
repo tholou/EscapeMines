b7b568bde30f1000cd405bbebeb3dd27b08b4d2c
Toluwalade Eyinade <ToluwaladeEyinade@kpmg.com.mt>
2021-01-06 01:46:41 +0100

    Include some unit test

---
+++ test/Betsson.EscapeMines.UnitTest/GameBuilderTests.cs
@@ -0,0 +1,53 @@
+﻿using Betsson.EscapeMines.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Betsson.EscapeMines.UnitTest
+{
+    [TestClass]
+    public class GameBuilderTests
+    {
+        private byte[] CreateStreamBytes()
+        {
+            string firstLine = "5 4";
+            string secondLine = "1,1 1,3 3,3";
+            string thirdLine = "4 2";
+            string fourthLine = "0 1 N";
+            string fifthLine = "M R M M M R M";
+            string sixthLine = "M R M M M M R M M M";
+            string seventhLine = "M R M M M M R M M";
+
+            var memoryStream = new MemoryStream();
+
+            var writer = new StreamWriter(memoryStream);
+            writer.WriteLine(firstLine);
+            writer.WriteLine(secondLine);
+            writer.WriteLine(thirdLine);
+            writer.WriteLine(fourthLine);
+            writer.WriteLine(fifthLine);
+            writer.WriteLine(sixthLine);
+            writer.WriteLine(seventhLine);
+            writer.Flush();
+
+            return memoryStream.ToArray();
+        }
+
+        [TestMethod]
+        public void GameBuilderShowResult()
+        {
+            var fileName = "gameSettings.txt";
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            var memoryStream = new MemoryStream(CreateStreamBytes());
+            var gameBuilder =  new GameBuilder(memoryStream, fileName);
+
+            gameBuilder.LoadData();
+
+            StringAssert.Contains(output.ToString(), "Mine Hit");
+            StringAssert.Contains(output.ToString(), "Still in Danger");
+            StringAssert.Contains(output.ToString(), "Success");
+        }
+    }
+}
---
+++ test/Betsson.EscapeMines.UnitTest/GameTests.cs
@@ -0,0 +1,104 @@
+﻿using Betsson.EscapeMines.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Betsson.EscapeMines.UnitTest
+{
+    [TestClass]
+    public class GameTests
+    {
+        private GameScene CreateGameScene(MineField mineField, Turtle turtle)
+        {
+            return new GameScene(mineField, turtle);
+        }
+
+        private MineField CreateMineField()
+        {
+            return new MineField()
+            {
+                Size = new int[] { 4, 5 },
+                MinesLocations = new List<int[]> { new int[] { 1, 1 }, new int[] { 1, 3 }, new int[] { 3, 3 } },
+                ExitPoint = new int[] { 2, 4 }
+            };
+        }
+
+        private Turtle CreateTurtle()
+        {
+            return new Turtle()
+            {
+                Position = new int[] { 1, 0 },
+                Direction = "N"
+            };
+        }
+
+        [TestMethod]
+        public void TestStartShouldReturnInvalidMoveIfAtLeastOneOfTheMovementsIsInvalid()
+        {
+            var game = CreateGameScene(CreateMineField(), CreateTurtle());
+            const string wrongMove = "W";
+            string moves = "L L M " + wrongMove;
+
+            var result = game.Run(moves.Split(" "));
+
+            Assert.AreEqual("Invalid Movement " + wrongMove, result);
+        }
+
+        [TestMethod]
+        public void TestStartShouldReturnInvalidTurtlePositionIfTurtlePositionIsInvalid()
+        {
+            var game = CreateGameScene(CreateMineField(), CreateTurtle());
+            string moves = "M M";
+
+            var result = game.Run(moves.Split(" "));
+
+            Assert.AreEqual("Invalid Turtle Position", result);
+        }
+
+        [TestMethod]
+        public void TestStartShouldReturnInvalidDirectionIfDirectionIsInvalid()
+        {
+            var turtle = CreateTurtle();
+            turtle.Direction = "J";
+            var mineField = CreateMineField();
+            var game = CreateGameScene(mineField, turtle);
+            string moves = "M M M R M M";
+
+            var result = game.Run(moves.Split(" "));
+
+            Assert.AreEqual("Invalid Direction", result);
+        }
+
+        [TestMethod]
+        public void TestStartShouldReturnSuccessIfTheTurtleHitsTheExitPoint()
+        {
+            var game = CreateGameScene(CreateMineField(), CreateTurtle());
+            string moves = "M R M M M M R M M";
+
+            var result = game.Run(moves.Split(" "));
+
+            Assert.AreEqual("Success", result);
+        }
+
+        [TestMethod]
+        public void TestStartShouldReturnStillInDangerIfTheTurtleDoesntHitBothTheExitPointNorTheMines()
+        {
+            var game = CreateGameScene(CreateMineField(), CreateTurtle());
+            string moves = "M R M M M M R M M M";
+
+            var result = game.Run(moves.Split(" "));
+
+            Assert.AreEqual("Still in Danger", result);
+        }
+
+        [TestMethod]
+        public void TestStartShouldReturnMineHitIfAnyMovementHitsAMine()
+        {
+            var game = CreateGameScene(CreateMineField(), CreateTurtle());
+            string moves = "M R M M M R M";
+
+            var result = game.Run(moves.Split(" "));
+
+            Assert.AreEqual("Mine Hit", result);
+        }
+    }
+}
